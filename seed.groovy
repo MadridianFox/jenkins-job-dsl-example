98ddd507404d56f241ff089f7db8a2ca7761b1cd
Koryukov Ivan <madridianfox@yandex.ru>
2025-07-20 22:12:13 +0300

    test

--- seed.groovy
+++ seed.groovy
@@ -1,20 +1,22 @@
-multibranchPipelineJob('cms') {
-    branchSources {
-        git {
-            id('cms')
-            remote('https://gitlab.com/greensight/ensi/cms/cms.git')
+folder("dev") {
+    multibranchPipelineJob('cms') {
+        branchSources {
+            git {
+                id('cms')
+                remote('https://gitlab.com/greensight/ensi/cms/cms.git')
+            }
         }
-    }
-    properties {
-        folderConfigFiles {
-            configs {
-                customConfig {
-                    id("env")
-                    name("env")
-                    comment("")
-                    content("""
-BANANA=ananas
-""")
+        properties {
+            folderConfigFiles {
+                configs {
+                    customConfig {
+                        id("env")
+                        name("env")
+                        comment("")
+                        content("""
+    BANANA=ananas
+    """)
+                    }
                 }
             }
         }
